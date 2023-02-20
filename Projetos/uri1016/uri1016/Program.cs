195314f7e5929211ea717e1160d521e3b6ad09ad
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-02-20 20:28:10 -0300

    realizando exercicio resolvido sobre o calculo de uma diagonal

---
+++ Projetos/uri1016/uri1016/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri1016 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            double min = number * 2;
+
+            Console.WriteLine(min + " minutos");
+        }
+    }
+}
---
+++ Projetos/uri1017/uri1017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1017 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double auto = 12;
+            int tempoViagem = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double media = tempoViagem * velocidade / auto;
+
+            Console.WriteLine(media.ToString("F3"));
+
+        }
+    }
+}

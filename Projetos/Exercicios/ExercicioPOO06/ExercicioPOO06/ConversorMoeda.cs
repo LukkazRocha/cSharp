1ee92aba3458e856147acf5cffbb93fab5973e74
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-02-23 17:51:36 -0300

    Realizando exercicio de fixação baseado em membros estáticos

--- Projetos/Exercicios/ExercicioPOO06/ExercicioPOO06/ConversorMoeda.cs
+++ Projetos/Exercicios/ExercicioPOO06/ExercicioPOO06/ConversorMoeda.cs
@@ -2,5 +2,10 @@
     internal class ConversorMoeda {
 
         public static double Iof = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao) {
+            double totalImposto = cotacao + (cotacao * Iof / 100);
+            return totalImposto * quantia;
+        }
     }
 }
--- Projetos/Exercicios/ExercicioPOO06/ExercicioPOO06/Program.cs
+++ Projetos/Exercicios/ExercicioPOO06/ExercicioPOO06/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExercicioPOO06 // Note: actual namespace depends on the project name.
 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Qual é a cotação em dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorEmReais = ConversorMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + valorEmReais.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }

d3ee3ac943127f8553a41556e99f976121facb30
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-03-01 20:53:14 -0300

    fazendo exercicio sobre Listas e aprenendo sobre matrizes

---
+++ Projetos/Praticas/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matrizes // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine(mat.Rank);
+
+            Console.WriteLine(mat.GetLength(0));
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}

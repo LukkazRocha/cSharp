c1e2fe048b70eebeb544b0657a9e705b682bfdaa
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-03-02 17:30:12 -0300

    realizando exercicio sobre matrizes e praticando datetime

---
+++ Projetos/Exercicios/ExercicioMatrizes/ExercicioMatrizes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ExercicioMatrizes // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            int[,] mat;
+
+            string[] matriz = Console.ReadLine().Split(' ');
+            int n = int.Parse(matriz[0]);
+            int m = int.Parse(matriz[1]);
+            mat = new int[n, m];
+
+            for (int i = 0; i < n; i++) {
+
+                string[] value = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < m; j++) {
+                    mat[i, j] = int.Parse(value[j]);
+                }
+            }
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+
+                for (int j = 0; j < m; j++) {
+                    if (mat[i, j] == number) {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0) {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }                        
+                        if (i > 0) {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < m - 1) {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < n - 1) {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Projetos/ExerciciosResolvidos/ExercicioResolvido12/ExercicioResolvido12/Program.cs
+++ Projetos/ExerciciosResolvidos/ExercicioResolvido12/ExercicioResolvido12/Program.cs
@@ -15,8 +15,26 @@ namespace ExercicioResolvido12 // Note: actual namespace depends on the project 
 
                 for (int j = 0; j < n; j++) {
                     mat[i, j] = int.Parse(values[j]);
+                }                
+            }
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            int negativeCount = 0;
+            for (int i = 0; i < n;  ++i) {
+
+                for (int j = 0; j < n; ++j) {
+                    if (mat[i, j] < 0) {
+                        negativeCount++;
+                    }
                 }
             }
+
+            Console.WriteLine();
+            Console.Write("Negative numbers = " + negativeCount);
         }
     }
 }

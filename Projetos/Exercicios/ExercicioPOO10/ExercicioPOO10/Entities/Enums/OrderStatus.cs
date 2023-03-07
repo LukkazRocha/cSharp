e90d19aa25a26757761dcd4ceb9067df160fc645
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2023-03-07 17:03:47 -0300

    Realizando exercicio para consolidar conhecimentos C# relacionados à Composição e Enumerações

---
+++ Projetos/Exercicios/ExercicioPOO10/ExercicioPOO10/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioPOO10.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
---
+++ Projetos/Exercicios/ExercicioPOO10/ExercicioPOO10/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace ExercicioPOO10.Entities
+{
+    internal class Product
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }        
+    }
+}

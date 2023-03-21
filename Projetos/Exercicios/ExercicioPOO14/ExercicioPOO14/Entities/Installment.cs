87a71fe64f42e52e9a305b290c0b1f3a75fff31f
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2023-03-21 16:06:11 -0300

    iniciando estudos sobre interface

---
+++ Projetos/Exercicios/ExercicioPOO14/ExercicioPOO14/Entities/Installment.cs
@@ -0,0 +1,14 @@
+﻿namespace ExercicioPOO14.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
--- Projetos/Praticas/SolucaoSemInterface/SolucaoSemInterface/Program.cs
+++ Projetos/Praticas/SolucaoSemInterface/SolucaoSemInterface/Program.cs
@@ -1,6 +1,5 @@
 ﻿using SolucaoSemInterface.Entities;
 using SolucaoSemInterface.Services;
-using System;
 using System.Globalization;
 
 namespace SolucaoSemInterface // Note: actual namespace depends on the project name.
@@ -9,7 +8,28 @@ namespace SolucaoSemInterface // Note: actual namespace depends on the project n
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (DD/MM/YYYY HH:SS): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (DD/MM/YYYY HH:SS): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+
         }
     }
 }

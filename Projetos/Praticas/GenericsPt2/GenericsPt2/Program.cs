80f8d7c2d892b84ed8ec2675ac400355af5de883
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2023-03-29 16:42:06 -0300

    Exemplo de generics com restrições

---
+++ Projetos/Praticas/GenericsPt2/GenericsPt2/Program.cs
@@ -0,0 +1,32 @@
+﻿using GenericsPt2.Services;
+using GenericsPt2.Entities;
+using System;
+using System.Globalization;
+
+namespace GenericsPt2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+            Console.WriteLine("Max:");
+            Console.WriteLine(max);
+        }
+    }
+}

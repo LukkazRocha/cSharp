7f020e537b08e01d2aa96f3a162f8f79517f82c4
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-05-25 15:21:31 -0300

    realizando exercício 01 sobre "EntradaDeDados"

---
+++ Algoritmos/Exercicios/01-EntradaDeDados/EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace EntradaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na casa?");
+            int rooms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha)");
+            string[] vect = Console.ReadLine().Split(' ');
+            string lastName = vect[0];
+            int age = int.Parse(vect[1]);
+            double height = double.Parse(vect[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(name);
+            Console.WriteLine(rooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

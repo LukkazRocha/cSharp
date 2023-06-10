d9428407e447d9b4e468a6ab6b83a9d65020acfa
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-06-10 17:26:54 -0300

    realizando dois exercicios propostos

---
+++ POO/Exercicios/Exercicio04/Exercicio04/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Exercicio04
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+
+        public double SalarioBruto { get; set; }
+
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

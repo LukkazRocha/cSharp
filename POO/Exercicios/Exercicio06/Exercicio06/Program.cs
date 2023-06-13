dbf4de6e9a154c7a463660ece63757e9d882be39
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-06-13 16:49:36 -0300

    Exercicio sobre membros estáticos

---
+++ POO/Exercicios/Exercicio06/Exercicio06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Exercicio06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultado = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

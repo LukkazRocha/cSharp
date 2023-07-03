5ba591d09bf1558d276f3f80f73754b14f502dd9
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2023-07-03 11:15:48 -0300

    feat: versao inicial do exercicio7

---
+++ POO/Exercicios/Exercicio07/Exercicio07/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace Exercicio07
+{
+    public class Conta
+    {
+        public int NumeroDaConta { get; private set; }
+
+        public string Titular { get; set; }
+
+        public double Saldo { get; set; }
+
+        public double DepositoInicial { get; set; }
+
+        public Conta(int numeroDaConta, string titular)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+        }
+
+        public Conta(int numeroDaConta, string titular, double depositoInicial) : this(numeroDaConta, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroDaConta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
